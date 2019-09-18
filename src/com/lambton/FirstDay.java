3ba5e9c897fb2405d92d4d64faad5ac84cd88d20
macstudent <rizulgoyal23@gmail.com>
2019-09-18 19:20:02 -0400

    commments

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,12 +5,17 @@ public class FirstDay
     public static void main(String[] args)
     {
         System.out.println("welcome to Java Programming");
-
+//variable declaration
         int a,b,c;
 
+        //value assignment
         a=10;
         b=20;
+
+        //perform add
         c=a+b;
+
+        //displayt result
         System.out.println("Sum is " + c);
 
     }
