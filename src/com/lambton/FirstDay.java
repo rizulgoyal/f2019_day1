eb47aed5547574ce3f1757404f2d4464bf06f95b
macstudent <rizulgoyal23@gmail.com>
2019-09-18 20:43:59 -0400

    functions

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -12,10 +12,31 @@ public class FirstDay
         a=10;
         b=20;
 
-        c=a+b;
+        c=a+b ;
 
         //displayt result
         System.out.println("Sum is " + c);
 
+        int x= Integer.MAX_VALUE;
+        System.out.println("x" + x);
+
+        int y= x+1;
+        System.out.println("y" +y);
+        System.out.println("min :" + Integer.MIN_VALUE);
+ Student s1 = new Student();
+ s1.setValues();
+ s1.display();
+
+ Student s2 = new Student();
+ s2.studentID=2;
+ s2.studentName="Harpreet Singh";
+
+ s2.display();
+
+        Student s3 = new Student();
+        s3.studentID=3;
+        s3.studentName="Anmol Sharma";
+
+        s3.display();
     }
 }
--- src/com/lambton/Student.java
+++ src/com/lambton/Student.java
@@ -4,4 +4,20 @@ public class Student {
     //variable define
     int studentID;
     String studentName;
+
+    void setValues()
+    {
+
+        studentID = 1;
+        studentName = "Rizul Goyal";
+    }
+
+    void display()
+    {
+
+        System.out.println("Student ID :" + studentID);
+        System.out.println("Student Name :" + studentName);
+    }
+
+
 }
